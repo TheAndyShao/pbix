db1313cbc9e6ed9868ceca45c6314f6d671ff149
Andy Shao <89639042+TheAndyShao@users.noreply.github.com>
2022-12-11 15:06:38 +0000

    Reordering methods

--- src/pbix/report.py
+++ src/pbix/report.py
@@ -17,58 +17,6 @@ class Report:
         self.field_set: set[str] = self.get_all_fields()
         self.updated: int = 0
 
-    def _read_layout(self, filepath: str) -> str:
-        """Return a cleaned JSON object of the layout file within the PBIX file."""
-        with zf.ZipFile(filepath, 'r') as zip_file:
-            data = zip_file.read('Report/Layout').decode("utf-16")
-            layout = json.loads(data)
-            return layout
-
-    def _read_modified_layout(self, filepath: str) -> str:
-        """Return a cleaned JSON object of the layout file within the PBIX file."""
-        with zf.ZipFile(filepath, 'r') as zip_file:
-            data = zip_file.read('Report/Layout').decode("utf-16")
-            string = data.replace(chr(0), "").replace(chr(28), "").replace(chr(29), "").replace(chr(25), "").replace("\"[", "[").replace("]\"", "]").replace("\"{", "{").replace("}\"", "}").replace("\\\\", "\\").replace("\\\"", "\"")
-            layout_modified = json.loads(string)
-            return layout_modified
-
-    def write_json_layout(self) -> None:
-        """Write the cleaned JSON object to file."""
-        with open('layout.json', 'w', encoding="utf-16") as outfile:
-            json.dump(self.layout_modified, outfile)
-
-    def update_measures(self, old: str, new: str) -> None:
-        """Iterates through pages and visuals in a pbix and replaces specified measure/column."""
-        print(f'Updating: {self.filename}')
-        for i, j, visual in self._generic_visuals_generator():
-            visual.update_measures(old, new)
-            self._update_visual_layout(i, j, visual.layout)
-            self.updated += visual.updated
-        if self.updated == 0:
-            print('No measures to update')
-
-    def _generic_visuals_generator(self) -> None:
-        """Generator for iterating through all visuals in a file."""
-        for i, page in enumerate(self.layout['sections']):
-            visuals = page['visualContainers']
-            for j, visual in enumerate(visuals):
-                yield i, j, GenericVisual(visual)
-
-    def _update_visual_layout(self, page: int, visual: int, layout: str) -> None:
-        """Updates visual layout with new definition."""
-        self.layout['sections'][page]['visualContainers'][visual] = layout
-
-    def update_slicers(self) -> None:
-        """Iterates through pages and genric visuals and updates slicers."""
-        for i, j, visual in self._generic_visuals_generator():
-            if visual.type == 'slicer':
-                slicer = Slicer(visual.layout)
-                slicer.unselect_all_items()
-                self._update_visual_layout(i, j, slicer.layout)
-                self.updated += slicer.updated
-        if self.updated == 0:
-            print('No slicers to update')
-
     def get_all_fields(self) -> None:
         """Get a list of used fields in the pbix file."""
         filters_path = parse(
@@ -95,6 +43,27 @@ class Report:
                 matches[field] = True
         return matches
 
+    def update_measures(self, old: str, new: str) -> None:
+        """Iterates through pages and visuals in a pbix and replaces specified measure/column."""
+        print(f'Updating: {self.filename}')
+        for i, j, visual in self._generic_visuals_generator():
+            visual.update_measures(old, new)
+            self._update_visual_layout(i, j, visual.layout)
+            self.updated += visual.updated
+        if self.updated == 0:
+            print('No measures to update')
+
+    def update_slicers(self) -> None:
+        """Iterates through pages and genric visuals and updates slicers."""
+        for i, j, visual in self._generic_visuals_generator():
+            if visual.type == 'slicer':
+                slicer = Slicer(visual.layout)
+                slicer.unselect_all_items()
+                self._update_visual_layout(i, j, slicer.layout)
+                self.updated += slicer.updated
+        if self.updated == 0:
+            print('No slicers to update')
+
     def write_file(self) -> None:
         """Writes the pbix json to file."""
         base, ext = os.path.splitext(self.filename)
@@ -111,6 +80,36 @@ class Report:
         os.remove(self.filepath)
         os.rename(temp_filepath, self.filepath)
 
+    def _read_layout(self, filepath: str) -> str:
+        """Return a cleaned JSON object of the layout file within the PBIX file."""
+        with zf.ZipFile(filepath, 'r') as zip_file:
+            data = zip_file.read('Report/Layout').decode("utf-16")
+            layout = json.loads(data)
+            return layout
+
+    def _read_modified_layout(self, filepath: str) -> str:
+        """Return a cleaned JSON object of the layout file within the PBIX file."""
+        with zf.ZipFile(filepath, 'r') as zip_file:
+            data = zip_file.read('Report/Layout').decode("utf-16")
+            string = data.replace(chr(0), "").replace(chr(28), "").replace(chr(29), "").replace(chr(25), "").replace("\"[", "[").replace("]\"", "]").replace("\"{", "{").replace("}\"", "}").replace("\\\\", "\\").replace("\\\"", "\"")
+            layout_modified = json.loads(string)
+            return layout_modified
+
+    def write_json_layout(self) -> None:
+        """Write the cleaned JSON object to file."""
+        with open('layout.json', 'w', encoding="utf-16") as outfile:
+            json.dump(self.layout_modified, outfile)
+
+    def _generic_visuals_generator(self) -> None:
+        """Generator for iterating through all visuals in a file."""
+        for i, page in enumerate(self.layout['sections']):
+            visuals = page['visualContainers']
+            for j, visual in enumerate(visuals):
+                yield i, j, GenericVisual(visual)
+
+    def _update_visual_layout(self, page: int, visual: int, layout: str) -> None:
+        """Updates visual layout with new definition."""
+        self.layout['sections'][page]['visualContainers'][visual] = layout
 
 class GenericVisual:
     """A base class to represent a generic visual object."""
@@ -124,24 +123,6 @@ class GenericVisual:
         self.title: str or None = self._return_visual_title()
         self.type: str or None = self._return_visual_type()
         self.updated: int = 0
-
-    def _parse_visual_option(self, visual_option: str) -> str or None:
-        """Returns a JSON object or None from visual option string"""
-        if visual_option in self.layout.keys():
-            return json.loads(self.layout[visual_option])
-        return None
-
-    def _return_visual_title(self) -> str or None:
-        """Return title of visual."""
-        title_path = parse("$..@.title[*].properties.text.expr.Literal.Value")
-        title = title_path.find(self.config)
-        return title[0].value if title else None
-
-    def _return_visual_type(self) -> str or None:
-        """Return type of visual."""
-        typ_path = parse("$.singleVisual.visualType")
-        typ = typ_path.find(self.config)
-        return typ[0].value if typ else None
 
     def update_measures(self, old: str, new: str) -> None:
         """Searches for relevant keys for measures and updates their value pairs."""
@@ -176,6 +157,23 @@ class GenericVisual:
 
                 print(f"Updated: {self.title}")
 
+    def _return_visual_title(self) -> str or None:
+        """Return title of visual."""
+        title_path = parse("$..@.title[*].properties.text.expr.Literal.Value")
+        title = title_path.find(self.config)
+        return title[0].value if title else None
+
+    def _return_visual_type(self) -> str or None:
+        """Return type of visual."""
+        typ_path = parse("$.singleVisual.visualType")
+        typ = typ_path.find(self.config)
+        return typ[0].value if typ else None
+
+    def _parse_visual_option(self, visual_option: str) -> str or None:
+        """Returns a JSON object or None from visual option string"""
+        if visual_option in self.layout.keys():
+            return json.loads(self.layout[visual_option])
+        return None
 
 class Slicer(GenericVisual):
     """A class representing a slicer."""
