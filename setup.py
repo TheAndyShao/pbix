b5118915f18c9e685c4639fc052ed0e8165fa627
TheAndyShao <89639042+TheAndyShao@users.noreply.github.com>
2022-12-28 22:31:38 +0000

    Incrementing version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import setuptools
 
 setuptools.setup(
     # Needed to silence warnings (and to be a worthwhile package)
-    name='pbix',
-    author='Andrew Shao',
-    author_email='andrewshao@hotmail.co.uk',
+    name="pbix",
+    author="Andrew Shao",
+    author_email="andrewshao@hotmail.co.uk",
     packages=setuptools.find_packages(where="src"),
     package_dir={"": "src"},
-    install_requires=['jsonpath-ng'],
-    version='0.1',
-    license='MIT',
-    description='Utilities for working with PBIX files',
+    install_requires=["jsonpath-ng"],
+    version="1.0",
+    license="MIT",
+    description="Utilities for working with PBIX files",
 )
